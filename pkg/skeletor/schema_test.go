99286694823232fc54f8cfdfb0f2c898efcb1374
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-09-04 17:44:09 -0500

    Implement other actions in schema
    Add tests to demonstrate how schema can be tested and inputs validated

--- pkg/skeletor/schema_test.go
+++ pkg/skeletor/schema_test.go
@@ -1,11 +1,14 @@
 package skeletor
 
 import (
+	"fmt"
 	"io/ioutil"
 	"testing"
 
+	"github.com/ghodss/yaml" // We are not using go-yaml because of serialization problems with jsonschema, don't use this library elsewhere
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/xeipuuv/gojsonschema"
 )
 
 func TestMixin_GetSchema(t *testing.T) {
@@ -32,4 +35,48 @@ func TestMixin_PrintSchema(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Equal(t, string(wantSchema), gotSchema)
+}
+
+func TestMixin_ValidateSchema(t *testing.T) {
+	m := NewTestMixin(t)
+
+	// Load the mixin schema
+	schemaB, err := m.GetSchema()
+	require.NoError(t, err)
+	schemaLoader := gojsonschema.NewStringLoader(schemaB)
+
+	// TODO: Add testcases for any other action inputs you have in testdata
+	// This validates that your schema.json is filled in properly
+	testcases := []struct {
+		name      string
+		file      string
+		wantError string
+	}{
+		{"install", "testdata/step-input.yaml", ""},
+		{"invalid property", "testdata/invalid-input.yaml", "Additional property args is not allowed"},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Read the mixin input as a go dump
+			mixinInputB, err := ioutil.ReadFile(tc.file)
+			require.NoError(t, err)
+			mixinInputMap := make(map[string]interface{})
+			err = yaml.Unmarshal(mixinInputB, &mixinInputMap)
+			require.NoError(t, err)
+			mixinInputLoader := gojsonschema.NewGoLoader(mixinInputMap)
+
+			// Validate the manifest against the schema
+			result, err := gojsonschema.Validate(schemaLoader, mixinInputLoader)
+			require.NoError(t, err)
+
+			if tc.wantError == "" {
+				assert.True(t, result.Valid())
+				assert.Empty(t, result.Errors())
+			} else {
+				assert.False(t, result.Valid())
+				assert.Contains(t, fmt.Sprintf("%v", result.Errors()), tc.wantError)
+			}
+		})
+	}
 }
